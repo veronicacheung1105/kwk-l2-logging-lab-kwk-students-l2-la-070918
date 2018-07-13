d28cdb91cbfe09f1f7557bd4db084015d3065c37
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 22:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const driver = 'bob';
 
 console.log('this code was called')
 
-console.log (`$(driver) is the driver variable name`)
+console.log (driver)
+
+console.log('is the driver variable name')
